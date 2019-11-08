0923fd99366ca6f756543db58b5331f631304e63
mateusz <biskup.mateusz1997@wp.pl>
2019-11-08 13:22:43 +0100

    Setup for upload to Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utills/geoCode')
 const forecast = require('./utills/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const viewPath = path.join(__dirname, '../tamplate/views')
 const partialsPath = path.join(__dirname, '../tamplate/partials')
@@ -91,7 +92,7 @@ app.get('*',(req,response) =>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port ' + port)
 })
